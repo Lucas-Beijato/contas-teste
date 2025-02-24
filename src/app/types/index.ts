05ca49e4a3a4d65c22a9a0291d2db7f9bf73bef5
lucasbeijato0 <lucasbeijato0@gmail.com>
2025-02-24 19:49:24 +0000

    remove coments and title

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -14,33 +14,4 @@ export type UpdateUserResponse = { message: string }
 export type TokenResponse = { token: string }
 export type ErrorResponse = { error: string }
 
-// type Adm_User_Type = {
-//     id: string,
-//     name: string,
-//     password: string
-// } | null;
-
-// type Account_Type = {
-//     id: string,
-//     user_id: string,
-//     description: string,
-//     limit_date: string,
-//     value: number,
-//     is_paid: boolean
-// }
-
-// type New_Account_Type = {
-//     userID: string,
-//     value: number,
-//     is_paid: boolean,
-//     description: string,
-//     limit_date: string
-// }
-
-// type Services_Return = {
-//     status: number,
-//     data: {},
-//     error: string | null
-// }
-
 
